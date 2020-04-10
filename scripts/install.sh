eee3313b319dd25c624f244ee094b7142c4907ac
nikosmonaut <nikosmonaut@protonmail.ch>
2020-04-10 21:04:47 +0200

    Remove useless files

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-npm install -g react-native-cli
-npm install -g detox-cli
-git submodule update --init --recursive
--- scripts/test.js
+++
@@ -1,9 +0,0 @@
-const exec = require('shell-utils').exec;
-
-run();
-
-function run() {
-  exec.execSync(`npm run test-js`);
-  exec.execSync(`npm run test-unit-ios`);
-  exec.execSync(`npm run test-unit-android`);
-}
